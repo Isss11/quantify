f10dfa11e4d301bea1a378452ea2806298007bfb
Isss11 <isaiah.sinclair.0711@gmail.com>
2025-12-18 19:49:34 -0500

    Add Weekend and Holiday Multiplier

--- stock_forecaster/forecaster/MaxStartDate.py
+++ stock_forecaster/forecaster/MaxStartDate.py
@@ -1,9 +1,12 @@
 from datetime import date, timedelta
 from .LSTMForecaster import TRAIN_PROPORTION
 
+# Used to increase offset relative to 2 weekend days, and 1 potential holiday
+WEEKEND_HOLIDAY_MULTIPLIER = 10/7
+
 def get_max_start_date(look_back):
     cur_date = date.today()
-    offset_days = int((2 + look_back)/(1 - TRAIN_PROPORTION))
+    offset_days = int(((2 + look_back)/(1 - TRAIN_PROPORTION)) * WEEKEND_HOLIDAY_MULTIPLIER)
 
     latest_start_date = cur_date - timedelta(days=offset_days)
     
